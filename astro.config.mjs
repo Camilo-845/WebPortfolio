45013aab90fbd46a034c7ce8d8d6feace9b01bb1
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-03-16 15:06:57 -0500

    astro-sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://camilo-845.github.io',
+  site: 'https://camilosar.site',
   base: '',
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 });
